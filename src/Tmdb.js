755ee4a4b540e7e2e6938984023efaa249566397
Maria Manoela <manoelamvc@hotmail.com>
2022-03-25 22:31:57 -0300

    Create home list and its categories

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,3 +1,49 @@
 const API_KEY = '8923785e7c2ade65987f7053882747d7';
 const API_BASE = 'https://api.themoviedb.org/3';
 
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais da Netflix',
+                items: []
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: []
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: []
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: []
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: []
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: []
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: []
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: []
+            }
+        ]
+    }
+}
