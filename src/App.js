1784d6f1c573f9de5d3d43153e3a03b1a7098a9f
Maria Manoela <manoelamvc@hotmail.com>
2022-03-26 16:10:34 -0300

    Implement useState to set movie list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
 
 export default() => {
+  const[ movieList, setMovieList] = useState([]);
+
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
     loadAll();
   }, []);
