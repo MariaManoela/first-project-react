44dcf6a2f5e17e8c24a66adc8027d675a9924ed9
Maria Manoela <manoelamvc@hotmail.com>
2022-03-26 19:12:01 -0300

    Create date object to take just the year of the date

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,9 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
     console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -14,7 +17,7 @@ export default ({item}) => {
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="featured--year">2099</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
                     <div className="featured--description">{item.overview}</div>
