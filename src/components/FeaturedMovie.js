b23fba69838092db4f2d59d92173c04e6e1cf917
Maria Manoela <manoelamvc@hotmail.com>
2022-03-26 19:04:41 -0300

    Add divs to featured

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,6 +2,7 @@ import React from "react";
 import './FeaturedMovie.css';
 
 export default ({item}) => {
+    console.log(item);
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -10,7 +11,17 @@ export default ({item}) => {
         }}>
             <div className="featured--vertical">
                 <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured-buttons">
 
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros:</strong> ...</div>
                 </div>
             </div>
         </section>
